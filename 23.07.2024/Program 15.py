557deee679ce75a6224f35b0c50895de9977961c
thiyaneshwars <138683953+thiyaneshwars@users.noreply.github.com>
2024-07-23 21:35:25 +0530

    Create Program 15.py

---
+++ 23.07.2024/Program 15.py
@@ -0,0 +1,15 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def remove_nth_from_end(head, n):
+    dummy = ListNode(0, head)
+    first = second = dummy
+    for _ in range(n + 1):
+        first = first.next
+    while first:
+        first = first.next
+        second = second.next
+    second.next = second.next.next
+    return dummy.next
