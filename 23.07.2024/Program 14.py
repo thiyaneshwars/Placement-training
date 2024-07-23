97dc729ea0f41dfd08deb5195a29737810667052
thiyaneshwars <138683953+thiyaneshwars@users.noreply.github.com>
2024-07-23 21:35:07 +0530

    Create Program 14.py

---
+++ 23.07.2024/Program 14.py
@@ -0,0 +1,18 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def merge_two_lists(l1, l2):
+    dummy = ListNode()
+    current = dummy
+    while l1 and l2:
+        if l1.val < l2.val:
+            current.next = l1
+            l1 = l1.next
+        else:
+            current.next = l2
+            l2 = l2.next
+        current = current.next
+    current.next = l1 if l1 else l2
+    return dummy.next
